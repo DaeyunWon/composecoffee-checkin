795d389126f18acff0b481d66467f667f3c2927f
dywon <dy.won@kt.com>
2026-04-24 15:54:08 +0900

    유틸 추가

--- ComposeCoffee/routes/admin.js
+++ ComposeCoffee/routes/admin.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const QRCode = require('qrcode');
 const config = require('../config');
 const { authenticate, requireAdmin } = require('../middleware/auth');
-const { getKSTDate, getKSTYear, getKSTMonth } = require('../utils/kst');
+const { getKSTNow, getKSTDate, getKSTYear, getKSTMonth } = require('../utils/kst');
 
 const router = express.Router();
 
